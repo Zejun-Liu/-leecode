61bdbf2eec40c5111bd2a4884f08bdd5e11aa056
Zejun-Liu <857591294@qq.com>
2019-03-21 11:01:38 +0800

    0321

---
+++ src/main/java/com/jiuxian/A20190321_03/Solution.java
@@ -0,0 +1,61 @@
+package com.jiuxian.A20190321_03;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 旋转数组
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ * <p>
+ * 输入: [-1,-100,3,99] 和 k = 2
+ * 输出: [3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ * 说明:
+ * <p>
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 要求使用空间复杂度为 O(1) 的原地算法。
+ * @see <a href="https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/23/"></a>
+ */
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = { 1, 2, 3, 1 };
+//        int[] nums = {1,2,3,4};
+//        int[] nums = {1,1,1,3,3,4,3,2,4,2 };
+        boolean b = solution.containsDuplicate(nums);
+        System.out.println(b);
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //慢
+    public boolean containsDuplicate2(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+        return false;
+    }
+}
