2183049f2769047797c84bf18f8e07dde2dba31a
jiuxian <857591294@qq.com>
2019-07-11 11:53:26 +0800

    反转链表、链表数据互换

---
+++ src/main/java/com/jiuxian/A20190710_01_ReverseLinkedList.java
@@ -0,0 +1,73 @@
+package com.jiuxian;
+
+/**
+ * @author: liuzejun
+ * *
+ * @email: 857591294@qq.com
+ * *
+ * @date: 2019-07-10 20:58:23
+ * *
+ * @comment: 206. 反转链表
+ * <p>
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class A20190710_01_ReverseLinkedList {
+
+    private static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode result = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode tmp = current.next;
+            current.next = result;
+            result = current;
+            current = tmp;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        node.next = node1;
+        ListNode node2 = new ListNode(3);
+        node1.next = node2;
+        node2.next = new ListNode(4);
+
+        System.out.println(node);
+        ListNode node4 = A20190710_01_ReverseLinkedList.reverseList(node);
+        System.out.println(node4);
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
