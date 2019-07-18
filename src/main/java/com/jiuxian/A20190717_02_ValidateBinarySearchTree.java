d54de6b1a4eb5aa80af069d9034fe44553f989c7
jiuxian <857591294@qq.com>
2019-07-18 11:04:36 +0800

    235 236

--- src/main/java/com/jiuxian/A20190717_02_ValidateBinarySearchTree.java
+++ src/main/java/com/jiuxian/A20190717_02_ValidateBinarySearchTree.java
@@ -1,9 +1,11 @@
 package com.jiuxian;
 
+import com.jiuxian.util.TreeNode;
+import com.jiuxian.util.TreeUtil;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * @author: liuzejun
@@ -94,63 +96,9 @@ public class A20190717_02_ValidateBinarySearchTree {
     }
 
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
-    }
-
-    public static TreeNode stringToTreeNode(String input) {
-        input = input.trim();
-        input = input.substring(1, input.length() - 1);
-        if (input.length() == 0) {
-            return null;
-        }
-
-        String[] parts = input.split(",");
-        String item = parts[0];
-        TreeNode root = new TreeNode(Integer.parseInt(item));
-        Queue<TreeNode> nodeQueue = new LinkedList<>();
-        nodeQueue.add(root);
-
-        int index = 1;
-        while (!nodeQueue.isEmpty()) {
-            TreeNode node = nodeQueue.remove();
-
-            if (index == parts.length) {
-                break;
-            }
-
-            item = parts[index++];
-            item = item.trim();
-            if (!item.equals("null")) {
-                int leftNumber = Integer.parseInt(item);
-                node.left = new TreeNode(leftNumber);
-                nodeQueue.add(node.left);
-            }
-
-            if (index == parts.length) {
-                break;
-            }
-
-            item = parts[index++];
-            item = item.trim();
-            if (!item.equals("null")) {
-                int rightNumber = Integer.parseInt(item);
-                node.right = new TreeNode(rightNumber);
-                nodeQueue.add(node.right);
-            }
-        }
-        return root;
-    }
-
     public static void main(String[] args) {
 
-        TreeNode root = stringToTreeNode("[5,1,4,null,null,3,6]");
+        TreeNode root = TreeUtil.getTree("[5,1,4,null,null,3,6]");
         boolean validBST = new A20190717_02_ValidateBinarySearchTree().isValidBST1(root);
         System.out.println(validBST);
     }
